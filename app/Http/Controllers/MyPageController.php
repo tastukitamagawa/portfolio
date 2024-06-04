e2a94b6cc3871c28b57d8ca844be50f61ced1d29
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-04 23:10:03 +0900

    更新プログラムの移動

--- app/Http/Controllers/MyPageController.php
+++ app/Http/Controllers/MyPageController.php
@@ -23,41 +23,4 @@ class MyPageController extends Controller
 
         return redirect('/login');
     }
-
-    public function update(Request $request){
-        // データベースに登録されている情報を取得
-        $currentUser = User::find(Auth::id());
-
-        
-        // バリデーション設定
-        $validated = $request->validate([
-            'username' => 'nullable|string',
-            'email' => 'nullable|string|unique:users,email',
-            'password' => 'nullable|string',
-            'new_password' => 'nullable|confirmed|min:8|regex:/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/'
-        ]);
-
-        // 更新された情報を格納する変数
-        $updatedFields = [];
-        if($request->username){
-            $currentUser->username = $validated['username'];
-            $updatedFields[] = "ユーザー情報";
-        } 
-        if($request->email){
-            $currentUser->email = $validated['email'];
-            $updatedFields[] = "メールドレス";
-        }
-        if(password_verify($validated['password'], $currentUser->password) && $request->new_password){
-            $currentUser->password = Hash::make($validated['new_password']);
-            $updatedFields[] = "パスワード";
-        }
-
-        $message = "";
-        if($updatedFields){
-            $message = implode("、", $updatedFields). "が更新されました。";
-        }
-
-        $currentUser->save();
-        return redirect()->back()->with('update_success', $message);
-    }
 }
