acc95fc6ec12ca2ef1ed10436d1ee85511e7bf3c
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-04 14:33:14 +0900

    usersテーブルにupdated_atとcreated_atの追加

---
+++ database/migrations/2024_06_04_142945_add_column_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('updated_at');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('updated_at');
+            $table->dropColumn('created_at');
+        });
+    }
+};
