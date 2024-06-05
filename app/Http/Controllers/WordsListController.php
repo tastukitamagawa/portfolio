4e12908a4ec907745d0a87bef80efe3cd8bb84ea
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-05 14:23:54 +0900

    昇順から降順に変更

--- app/Http/Controllers/WordsListController.php
+++ app/Http/Controllers/WordsListController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class WordsListController extends Controller
 {
     public function create(){
-        $list = Dictionary::where('user_id', auth()->id())->with('word')->get();
+        $list = Dictionary::where('user_id', auth()->id())->with('word')->orderBy('updated_at', 'desc')->get();
         return view('/index', compact('list'));
     }
 }
