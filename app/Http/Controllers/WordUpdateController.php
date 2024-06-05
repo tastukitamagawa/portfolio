7d184775eb5a9087b54f59e881510f02d02de4ea
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-06 00:07:14 +0900

    セッションの追加、更新処理の作成

--- app/Http/Controllers/WordUpdateController.php
+++ app/Http/Controllers/WordUpdateController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Word;
 use Illuminate\Http\Request;
 
 class WordUpdateController extends Controller
 {
-    //
-    public function create(){
-        return view('/word-update');
+    public function create(Request $request){
+        session(['word_id' => request()->query('id')]);
+        $word = Word::where('word_id', session('word_id'))->firstOrFail();
+        // セッションに保存
+        return view('/word-update', compact('word'));
+    }
+    
+    public function update(Request $request){
+        // 現在表示されている単語
+        // dd(session('word_id'));
+        $currentWord = Word::where('word_id', session('word_id'))->firstOrFail();
+        $validated = $request->validate([
+            'word' => 'nullable|string|regex:/^[a-zA-z]+$/',
+            'meaning' => 'nullable|string|regex:/^[a-zA-Z,.&\s\n]+$/',
+        ]);
+
+        $currentWord->word = $validated['word'];
+        // if($request->word){
+        // }
+
+        $currentWord->save();
+
+        return redirect('/');
     }
 }
