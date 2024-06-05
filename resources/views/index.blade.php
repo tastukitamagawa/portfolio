6be0ab52796e8f62a3dc578c95880c200ed8e0de
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-05 15:48:04 +0900

    セレクトタグの修正とページネーションの追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,15 +13,16 @@
         <button class="dictionary-form__button" type="submit text--ja">再生する</button>
     </form>
 
-    <form class="word-list-setting" action="">
+    <form class="word-list-setting" action="{{route('listLimit')}}" method="GET">
         <div class="word-list-setting__input-wrap">
-            <select class="word-list-setting__input" name="" id="">
-                <option value="10">10</option>
-                <option value="10">30</option>
-                <option value="10">50</option>
-                <option value="10">100</option>
+            <select class="word-list-setting__input" name="limit" id="">
+                <option value="10" {{$limit == 10 ? 'selected' : ''}}>10</option>
+                <option value="30" {{$limit == 30 ? 'selected' : ''}}>30</option>
+                <option value="50" {{$limit == 50 ? 'selected' : ''}}>50</option>
+                <option value="100" {{$limit == 100 ? 'selected' : ''}}>100</option>
             </select>
         </div>
+        <button class="word-list-setting__button button" type="submit">表示する</button>
     </form>
 
     <ul class="word-list">
@@ -32,5 +33,8 @@
             </li>
         @endforeach
     </ul>
+    <div class="pagination">
+        {{$list->links()}}
+    </div>
 @endsection('content')
 
