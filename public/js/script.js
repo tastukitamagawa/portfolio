2da477859cb3b8b9a1ddbe4a6a7047fe995c4e42
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-08 16:46:02 +0900

    音声読み上げの実装

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,24 @@
-// let userSetting = document.getElementsByClassName("mypage-setting__icon");
-// let modalBg = document.querySelector(".modal__bg");
-// let modalCancelBtn = document.querySelectorAll(".modal-cancel-button");
-// const html = document.querySelector("html");
-// let scrollPosition = 0;
+let url = location.href;
+const indexText = url.indexOf('/words/');
+const wordVoice = document.getElementById('word-voice');
+const meaningVoice = document.getElementById('meaning-voice');
 
-
-// // ユーザー情報更新アイコンをクリックして、モーダルを開く
-// for(let i = 0; i < userSetting.length; i++){
-//     userSetting[i].addEventListener("click",  function(){
-//         let data = userSetting[i].dataset.user_update
-//         modalBg = document.getElementById(data);
-//         modalBg.classList.add("is--show");
+window.onload = () => {
+    if(indexText > -1){
         
-//         scrollPosition = document.documentElement.scrollTop;
-//         html.style.position = "fixed";
-//         html.style.top = `-${scrollPosition}px`
-//     });
-// }
+        // 音声の再生
+        const wordPlay = () => wordVoice.play();
+        const meaningPlay = () => meaningVoice.play();
+        const redirect = () => window.location.href = '/';;
 
-// // モーダルのキャンセルボタンをクリックすると、モーダルが閉じる
-// modalCancelBtn.forEach(function(btn){
-//     btn.addEventListener("click", function(){
-//         modalBg.classList.remove("is--show");
-//         html.style.position = "";
-//         html.style.top = "";
-//         window.scrollTo(0, scrollPosition);
-//     });
-// })
+        const play = () => {
+            wordPlay();
+            wordVoice.addEventListener('ended', () => {
+                setTimeout(meaningPlay, 2000);
+            });
+        }
+
+        // 2秒経過したら再生する
+        setTimeout(play, 1000);
+    }
+}
