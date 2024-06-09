ee39920e666617d25a9618cd0b54d73f554b4241
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-09 21:18:39 +0900

    jsonの作成とキューにジョブを渡す

--- app/Http/Controllers/WordsController.php
+++ app/Http/Controllers/WordsController.php
@@ -2,74 +2,34 @@
 
 namespace App\Http\Controllers;
 
-require __DIR__ . '/../../../vendor/autoload.php';
-
 use App\Models\Dictionary;
-use Illuminate\Http\Request;
-use Google\Cloud\TextToSpeech\V1\AudioConfig;
-use Google\Cloud\TextToSpeech\V1\AudioEncoding;
-use Google\Cloud\TextToSpeech\V1\SynthesisInput;
-use Google\Cloud\TextToSpeech\V1\TextToSpeechClient;
-use Google\Cloud\TextToSpeech\V1\VoiceSelectionParams;
-use Google\Cloud\TextToSpeech\V1\SsmlVoiceGender;
+use App\Jobs\GenerateAudioFiles;
 
 class WordsController extends Controller
 {
     public function create($id){
         $word = Dictionary::where("user_id", auth()->id())->where('word_id', $id)->with('word')->orderBy('created_at')->first();
+        return view('words', compact('word'));
+    }
 
-        // 環境変数に値を入れる
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/../../../text-to-speak.json');
+    public function getWords(){
+        $words = Dictionary::where("user_id", auth()->id())->with('word')->orderBy('created_at')->get();
+        $chunks = array_chunk($words->toArray(), 5);
 
-        // 新しくクライアントを設定する
-        $wordClient = new TextToSpeechClient();
-        $meaningClient = new TextToSpeechClient();
-
-        // 音声化対象のテキストの設定
-        $wordInput = new SynthesisInput();
-        $wordInput->setText($word->word->word);
-        $meaningInput = new SynthesisInput();
-        $meaningInput->setText($word->word->meaning);
-
-        // 音声の設定
-        $wordVoice = new VoiceSelectionParams();
-        $wordVoice->setLanguageCode('en-US');
-        $wordVoice->setSsmlGender(SsmlVoiceGender::FEMALE);
-        $meaningVoice = new VoiceSelectionParams();
-        $meaningVoice->setLanguageCode('en-US');
-        $meaningVoice->setSsmlGender(SsmlVoiceGender::MALE);
-        
-        // ファイル形式の設定
-        $wordAudioConfig = new AudioConfig();
-        $wordAudioConfig->setAudioEncoding(AudioEncoding::MP3);
-        $meaningAudioConfig = new AudioConfig();
-        $meaningAudioConfig->setAudioEncoding(AudioEncoding::MP3);
-        
-        // オプションの指定
-        $wordResponse = $wordClient->synthesizeSpeech($wordInput, $wordVoice, $wordAudioConfig);
-        $meaningResponse = $meaningClient->synthesizeSpeech($meaningInput, $meaningVoice, $meaningAudioConfig);
-        
-        // 音声データーの取得
-        $wordAudioContent = $wordResponse->getAudioContent();
-        $meaningAudioContent = $meaningResponse->getAudioContent();
-        
-        // フォルダの作成
-        $outputDir = storage_path('app/public/voice');
-        if(!file_exists($outputDir)){
-            mkdir($outputDir, 0777, true);
+        // ハッシュマップ
+        $hashmap = [];
+        foreach($words as $word){
+            $hashmap[] = [
+                'word_id' => $word->word->word_id,
+                'word' => $word->word->word,
+                'meaning' => $word->word->meaning,
+            ];
         }
 
-        // 保存するファイル
-        $wordOutputPath = $outputDir . '/word.mp3';
-        $meaningOutputPath = $outputDir . '/meaning.mp3';
+        foreach($chunks as $chunk){
+            GenerateAudioFiles::dispatch($chunk)->onQueue('audio_generation');
+        }
 
-        // データをファイルの書き込む
-        $wordResult = file_put_contents($wordOutputPath, $wordAudioContent);
-        $meaningResult = file_put_contents($meaningOutputPath, $meaningAudioContent);
-        
-        $wordClient->close();
-        $meaningClient->close();
-        
-        return view('words', compact('word'));
+        return response()->json($hashmap);
     }
 }
