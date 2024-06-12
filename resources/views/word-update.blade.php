4378a49e48bc23df1067a3ee48c8d5012d73fc65
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-13 08:15:34 +0900

    クラス名追加、ルートの変更

--- resources/views/word-update.blade.php
+++ resources/views/word-update.blade.php
@@ -9,8 +9,9 @@
                 <p class="update-alert-error">{{$errors->first('word_update_fail')}}</p>
             </div>
         @endif
-        <form action="{{route('wordEdit')}}" class="word-register-form" method="POST">
+        <form action="{{route('wordEdit', ['word_id' => $word->word_id])}}" class="word-register-form" method="POST">
             @csrf
+            @method('PATCH')
             <div class="word-register-form__input-wrap input-wrap">
                 <label for="word">
                     <span class="word-register-form__text">Word</span>
@@ -25,13 +26,13 @@
             </div>
 
             <div class="word-register-form__button-area form-button-area">
-                <button class="word-register-form__button button" type="submit">修正する</button>
+                <button class="word-register-form__button button text--ja" type="submit">修正する</button>
             </div>
         </form>
-        <form action="{{route('wordDelete')}}" method="POST">
+        <form action="{{route('wordDelete', ['word_id' => $word->word_id])}}" method="POST">
             @csrf
             @method('DELETE')
-            <button class="word-delete-form__button button border-button" type="submit">削除する</button>
+            <button class="word-delete-form__button button border-button text--ja" type="submit">削除する</button>
         </form>
     </div>
 
