7b97bbb4005c6fde386e6ba792fe1554aa2c93bf
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-13 08:55:47 +0900

    try・catchの追加

--- app/Http/Controllers/UserUpdateController.php
+++ app/Http/Controllers/UserUpdateController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class UserUpdateController extends Controller
 {
@@ -15,59 +16,63 @@ class UserUpdateController extends Controller
     } 
 
     public function update(Request $request){
-        // データベースに登録されている情報を取得
-        $currentUser = User::find(Auth::id());
+        try{
+            // データベースに登録されている情報を取得
+            $currentUser = User::find(Auth::id());
 
-        // バリデーション設定
-        $validated = $request->validate([
-            'username' => 'nullable|string',
-            'email' => 'nullable|string|unique:users,email',
-            'password' => 'nullable|string',
-            'new_password' => 'nullable|confirmed|min:8|regex:/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/'
-        ]);
- 
-        // 更新された情報を格納する変数
-        $updatedFields = [];
-        $inputFields = ['username', 'email', 'password', 'new_password', 'new_password_confirmation'];
-        // 入力が全て未入力の時の処理
-        $allFieldsEmpty = true;
-        foreach($inputFields as $inputField){
-            // 入力されていたらfalseにする
-            if($request->filled($inputField)){
-                $allFieldsEmpty = false;
-                break;
+            // バリデーション設定
+            $validated = $request->validate([
+                'username' => 'nullable|string',
+                'email' => 'nullable|string|unique:users,email',
+                'password' => 'nullable|string',
+                'new_password' => 'nullable|confirmed|min:8|regex:/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/'
+            ]);
+
+            // 更新された情報を格納する変数
+            $updatedFields = [];
+            $inputFields = ['username', 'email', 'password', 'new_password', 'new_password_confirmation'];
+            // 入力が全て未入力の時の処理
+            $allFieldsEmpty = true;
+            foreach($inputFields as $inputField){
+                // 入力されていたらfalseにする
+                if($request->filled($inputField)){
+                    $allFieldsEmpty = false;
+                    break;
+                }
+            } 
+
+            if($allFieldsEmpty){
+                redirect()->back()->withErrors(["current_password_error" => "更新したい項目を入力してください。"]);
             }
-        } 
 
-        if($allFieldsEmpty){
-            redirect()->back()->withErrors(["current_password_error" => "更新したい項目を入力してください。"]);
-        }
+            if($request->username){
+                $currentUser->username = $validated['username'];
+                $updatedFields[] = "ユーザー名";
+            } 
+            if($request->email){
+                $currentUser->email = $validated['email'];
+                $updatedFields[] = "メールドレス";
+            }
+            if(isset($request->password) && $request->new_password){
+                $currentUser->password = Hash::make($validated['new_password']);
+                $updatedFields[] = "パスワード";
+            } else if(isset($request->password) && password_verify($validated['password'], $currentUser->password) === false){
+                return redirect()->back()->withErrors(["current_password_error" => "現在のパスワードが正しくありません。"]);
+            } else if(!isset($request->password) && isset($request->new_password)){
+                return redirect()->back()->withErrors(["current_password_error" => "現在のパスワードを入力してください。"]);
+            } else if(isset($request->password) && !isset($request->new_password)){
+                return redirect()->back()->withErrors(["current_password_error" => "新しいパスワードを設定してください。"]);
+            }
 
-        if($request->username){
-            $currentUser->username = $validated['username'];
-            $updatedFields[] = "ユーザー名";
-        } 
-        if($request->email){
-            $currentUser->email = $validated['email'];
-            $updatedFields[] = "メールドレス";
-        }
-        if(isset($request->password) && $request->new_password){
-            $currentUser->password = Hash::make($validated['new_password']);
-            $updatedFields[] = "パスワード";
-        } else if(isset($request->password) && password_verify($validated['password'], $currentUser->password) === false){
-            return redirect()->back()->withErrors(["current_password_error" => "現在のパスワードが正しくありません。"]);
-        } else if(!isset($request->password) && isset($request->new_password)){
-            return redirect()->back()->withErrors(["current_password_error" => "現在のパスワードを入力してください。"]);
-        } else if(isset($request->password) && !isset($request->new_password)){
-            return redirect()->back()->withErrors(["current_password_error" => "新しいパスワードを設定してください。"]);
-        }
+            $message = "";
+            if($updatedFields){
+                $message = implode("、", $updatedFields). "が更新されました。";
+            }
 
-        $message = "";
-        if($updatedFields){
-            $message = implode("、", $updatedFields). "が更新されました。";
+            $currentUser->save();
+            return redirect()->back()->with('update_success', $message);
+        } catch(\Exception $e){
+            Log::error('Error updating word: ' . $e->getMessage());
         }
-
-        $currentUser->save();
-        return redirect()->back()->with('update_success', $message);
     }
 }
