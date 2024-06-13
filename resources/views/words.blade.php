ba8b97e55d86c066a4c245e7f170c917907ecc50
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-13 11:32:38 +0900

    オーディオを非表示に

--- resources/views/words.blade.php
+++ resources/views/words.blade.php
@@ -6,10 +6,10 @@
     <div class="word-box box">
         <h2 class="word-box__title" id="word">{{$word->word->word}}</h2>
         <p class="word-box__meaning" id="meaning">{{$word->word->meaning}}</p>
-        <audio class="voice" id="word-voice" controls>
+        <audio class="voice" id="word-voice">
             <source src="{{asset('storage/voice/word'.$word->word->word_id.'.mp3')}}" type="audio/mpeg">
         </audio>
-        <audio class="voice" id="meaning-voice" controls>
+        <audio class="voice" id="meaning-voice">
             <source src="{{asset('storage/voice/meaning'.$word->word->word_id.'.mp3')}}" type="audio/mpeg">
         </audio>
         <div class="word-box__operation-area">
