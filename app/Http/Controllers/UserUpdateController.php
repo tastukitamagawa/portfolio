653a02e30b9318c088476b6d9501740d5d449f0f
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-14 14:11:48 +0900

    try・catchの削除

--- app/Http/Controllers/UserUpdateController.php
+++ app/Http/Controllers/UserUpdateController.php
@@ -10,69 +10,64 @@ use Illuminate\Support\Facades\Log;
 
 class UserUpdateController extends Controller
 {
-    //
     public function create(){
         return view('profile-update'); 
     } 
 
     public function update(Request $request){
-        try{
-            // データベースに登録されている情報を取得
-            $currentUser = User::find(Auth::id());
+        // データベースに登録されている情報を取得
+        $currentUser = User::find(Auth::id());
 
-            // バリデーション設定
-            $validated = $request->validate([
-                'username' => 'nullable|string',
-                'email' => 'nullable|string|unique:users,email',
-                'password' => 'nullable|string',
-                'new_password' => 'nullable|confirmed|min:8|regex:/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/'
-            ]);
+        // バリデーション設定
+        $validated = $request->validate([
+            'username' => 'nullable|string',
+            'email' => 'nullable|string|unique:users,email',
+            'password' => 'nullable|string',
+            'new_password' => 'nullable|confirmed|min:8|regex:/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/'
+        ]);
 
-            // 更新された情報を格納する変数
-            $updatedFields = [];
-            $inputFields = ['username', 'email', 'password', 'new_password', 'new_password_confirmation'];
-            // 入力が全て未入力の時の処理
-            $allFieldsEmpty = true;
-            foreach($inputFields as $inputField){
-                // 入力されていたらfalseにする
-                if($request->filled($inputField)){
-                    $allFieldsEmpty = false;
-                    break;
-                }
-            } 
-
-            if($allFieldsEmpty){
-                redirect()->back()->withErrors(["current_password_error" => "更新したい項目を入力してください。"]);
+        // 更新された情報を格納する変数
+        $updatedFields = [];
+        $inputFields = ['username', 'email', 'password', 'new_password', 'new_password_confirmation'];
+        // 入力が全て未入力の時の処理
+        $allFieldsEmpty = true;
+        foreach($inputFields as $inputField){
+            // 入力されていたらfalseにする
+            if($request->filled($inputField)){
+                $allFieldsEmpty = false;
+                break;
             }
+        } 
 
-            if($request->username){
-                $currentUser->username = $validated['username'];
-                $updatedFields[] = "ユーザー名";
-            } 
-            if($request->email){
-                $currentUser->email = $validated['email'];
-                $updatedFields[] = "メールドレス";
-            }
-            if(isset($request->password) && $request->new_password){
-                $currentUser->password = Hash::make($validated['new_password']);
-                $updatedFields[] = "パスワード";
-            } else if(isset($request->password) && password_verify($validated['password'], $currentUser->password) === false){
-                return redirect()->back()->withErrors(["current_password_error" => "現在のパスワードが正しくありません。"]);
-            } else if(!isset($request->password) && isset($request->new_password)){
-                return redirect()->back()->withErrors(["current_password_error" => "現在のパスワードを入力してください。"]);
-            } else if(isset($request->password) && !isset($request->new_password)){
-                return redirect()->back()->withErrors(["current_password_error" => "新しいパスワードを設定してください。"]);
-            }
-
-            $message = "";
-            if($updatedFields){
-                $message = implode("、", $updatedFields). "が更新されました。";
-            }
-
-            $currentUser->save();
-            return redirect()->back()->with('update_success', $message);
-        } catch(\Exception $e){
-            Log::error('Error updating word: ' . $e->getMessage());
+        if($allFieldsEmpty){
+            redirect()->back()->withErrors(["current_password_error" => "更新したい項目を入力してください。"]);
         }
+
+        if($request->username){
+            $currentUser->username = $validated['username'];
+            $updatedFields[] = "ユーザー名";
+        } 
+        if($request->email){
+            $currentUser->email = $validated['email'];
+            $updatedFields[] = "メールドレス";
+        }
+        if(isset($request->password) && $request->new_password){
+            $currentUser->password = Hash::make($validated['new_password']);
+            $updatedFields[] = "パスワード";
+        } else if(isset($request->password) && password_verify($validated['password'], $currentUser->password) === false){
+            return redirect()->back()->withErrors(["current_password_error" => "現在のパスワードが正しくありません。"]);
+        } else if(!isset($request->password) && isset($request->new_password)){
+            return redirect()->back()->withErrors(["current_password_error" => "現在のパスワードを入力してください。"]);
+        } else if(isset($request->password) && !isset($request->new_password)){
+            return redirect()->back()->withErrors(["current_password_error" => "新しいパスワードを設定してください。"]);
+        }
+
+        $message = "";
+        if($updatedFields){
+            $message = implode("、", $updatedFields). "が更新されました。";
+        }
+
+        $currentUser->save();
+        return redirect()->back()->with('update_success', $message);
     }
 }
--- app/Http/Controllers/WordUpdateController.php
+++ app/Http/Controllers/WordUpdateController.php
@@ -11,54 +11,38 @@ use Illuminate\Support\Facades\Route;
 class WordUpdateController extends Controller
 {
     public function create(Request $request, $word_id = null){
-        try{
-            $word = Word::where('word_id', $word_id)->firstOrFail();
-            return view('/word-update', compact('word'));
-        } catch(\Exception $e){
-            Log::error('Error updating word: ' . $e->getMessage());
-        }
+        $word = Word::where('word_id', $word_id)->firstOrFail();
+        return view('/word-update', compact('word'));
     }
     
     public function update(Request $request, $word_id = null){
-        try{
-            // 現在表示されている単語
-            $currentWord = Word::where('word_id', $word_id)->firstOrFail();
-            $validated = $request->validate([
-                'word' => 'nullable|string|regex:/^[a-zA-Z]+$/',
-                'meaning' => 'nullable|string|regex:/^[\s\S]*$/',
-            ]);
+        // 現在表示されている単語
+        $currentWord = Word::where('word_id', $word_id)->firstOrFail();
+        $validated = $request->validate([
+            'word' => 'nullable|string|regex:/^[a-zA-Z]+$/',
+            'meaning' => 'nullable|string|regex:/^[\s\S]*$/',
+        ]);
 
-            $isChanged = false;
-            if($validated['word'] != null &&  $currentWord->word != $validated['word']){
-                $currentWord->word = $validated['word'];
-                $isChanged = true;
-            }
-            if($validated['meaning'] != null && $currentWord->meaning != $validated['meaning']){
-                $currentWord->meaning = $validated['meaning'];
-                $isChanged = true;
-            }
-            if(!$isChanged){
-                return back()->withErrors(['word_update_fail' => '更新したい項目を変更してください。']);
-            }
-
-            $currentWord->save();
-
-            return redirect()->route('top');
-        } catch (\Illuminate\Validation\ValidationException $e) {
-            Log::error('Validation error: ' . json_encode($e->errors()));
-            return back()->withErrors($e->errors());
-        } catch(\Exception $e) {
-            Log::error('Error updating word: ' . $e->getMessage());
-            return back()->withErrors(['word_update_fail' => 'エラーが発生しました。もう一度お試しください。']);
+        $isChanged = false;
+        if($validated['word'] != null &&  $currentWord->word != $validated['word']){
+            $currentWord->word = $validated['word'];
+            $isChanged = true;
         }
+        if($validated['meaning'] != null && $currentWord->meaning != $validated['meaning']){
+            $currentWord->meaning = $validated['meaning'];
+            $isChanged = true;
+        }
+        if(!$isChanged){
+            return back()->withErrors(['word_update_fail' => '更新したい項目を変更してください。']);
+        }
+
+        $currentWord->save();
+
+        return redirect()->route('top');
     }
 
     public function delete($word_id = null){
-        try{
-            Word::where('word_id', $word_id)->delete();
-            return redirect()->route('top');
-        } catch(\Exception $e){
-            Log::error('Error updating word: ' . $e->getMessage());
-        }
+        Word::where('word_id', $word_id)->delete();
+        return redirect()->route('top');
     }
 }
