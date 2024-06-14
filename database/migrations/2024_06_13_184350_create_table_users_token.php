4f565a7a95b4a47cabaa7b85667cc854430949d4
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-14 09:48:28 +0900

    マイグレーション内容の修正

--- database/migrations/2024_06_13_184350_create_table_users_token.php
+++ database/migrations/2024_06_13_184350_create_table_users_token.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('guest_tokens', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->default(1);
             $table->string('token')->unique();
             $table->timestamp('updated_at');
             $table->timestamp('created_at');
