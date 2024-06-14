4946ed848052a70b25357b9c25389a7e8be11fb2
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-14 14:44:34 +0900

    try・catchの削除

--- app/Http/Controllers/WordsController.php
+++ app/Http/Controllers/WordsController.php
@@ -10,43 +10,35 @@ use Illuminate\Support\Facades\Log;
 class WordsController extends Controller
 {
     public function create(Request $request){
-        try{
-            $amount = $request->input('amount') ? (int)$request->input('amount') : Dictionary::where("user_id", auth()->id())->count();
-            session(['limitAmount' => $amount]);
-            $word = Dictionary::where("user_id", auth()->id())->with('word')->orderBy('created_at')->first();
-            if($word){
-                return view('words', compact('word'));
-            } else{
-                return redirect()->route('wordsRegister');
-            }
-        } catch(\Exception $e){
-            Log::error('Error updating word: ' . $e->getMessage());
+        $amount = $request->input('amount') ? (int)$request->input('amount') : Dictionary::where("user_id", auth()->id())->count();
+        session(['limitAmount' => $amount]);
+        $word = Dictionary::where("user_id", auth()->id())->with('word')->orderBy('created_at')->first();
+        if($word){
+            return view('words', compact('word'));
+        } else{
+            return redirect()->route('wordsRegister');
         }
     }
 
     public function getWords(){
-        try{
-            $limitAmount = session()->get('limitAmount');
-            $words = Dictionary::where("user_id", auth()->id())->with('word')->orderBy('created_at')->limit($limitAmount)->get();
-            $chunks = array_chunk($words->toArray(), 5);
-    
-            // ハッシュマップ
-            $hashmap = [];
-            foreach($words as $word){
-                $hashmap[] = [
-                    'word_id' => $word->word->word_id,
-                    'word' => $word->word->word,
-                    'meaning' => $word->word->meaning,
-                ];
-            }
-    
-            foreach($chunks as $chunk){
-                GenerateAudioFiles::dispatch($chunk)->onQueue('audio_generation');
-            }
-    
-            return response()->json($hashmap);
-        } catch(\Exception $e){
-            Log::error('Error updating word: ' . $e->getMessage());
+        $limitAmount = session()->get('limitAmount');
+        $words = Dictionary::where("user_id", auth()->id())->with('word')->orderBy('created_at')->limit($limitAmount)->get();
+        $chunks = array_chunk($words->toArray(), 5);
+
+        // ハッシュマップ
+        $hashmap = [];
+        foreach($words as $word){
+            $hashmap[] = [
+                'word_id' => $word->word->word_id,
+                'word' => $word->word->word,
+                'meaning' => $word->word->meaning,
+            ];
         }
+
+        foreach($chunks as $chunk){
+            GenerateAudioFiles::dispatch($chunk)->onQueue('audio_generation');
+        }
+
+        return response()->json($hashmap);
     }
 }
