4b8e35d97a4ded3fc749a83c216aeb4325a5c271
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-14 14:04:46 +0900

    ルート名の変更

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,22 +14,18 @@ class UserController extends Controller
     }   
 
     public function store(Request $request){
-        try{
-            $validated = $request->validate([
-                'username' => 'required|string',
-                'email' => 'required|string|unique:users,email',
-                'password' => 'required|confirmed|min:8|regex:/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/'
-            ]);
-    
-            $user = new User();
-            $user->username = $validated['username']; // ユーザー名をセット
-            $user->email = $validated['email']; // メールアドレスをセット
-            $user->password = Hash::make($validated['password']); // パスワードをハッシュ化してセット
-            $user->save();
-    
-            return redirect()->route('login.login')->with('register_success', '登録が完了しました。ログインできます。');
-        } catch(\Exception $e){
-            Log::error('Error updating word: ' . $e->getMessage());
-        }
+        $validated = $request->validate([
+            'username' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|confirmed|min:8|regex:/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/'
+        ]);
+
+        $user = new User();
+        $user->username = $validated['username']; // ユーザー名をセット
+        $user->email = $validated['email']; // メールアドレスをセット
+        $user->password = Hash::make($validated['password']); // パスワードをハッシュ化してセット
+        $user->save();
+
+        return redirect()->route('userLogin')->with('register_success', '登録が完了しました。ログインできます。');
     }
 }
