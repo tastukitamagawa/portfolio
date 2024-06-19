8d9ceee8f88ac58f19e4733a1e63560a8531d70b
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-19 09:57:39 +0900

    Revert "usersテーブルにupdated_atとcreated_atにデフォルト値を追加"
    This reverts commit 0cdb2f59973fffb8f8d65241c9532f3854097ecc.

--- database/migrations/2024_06_04_142945_add_column_user.php
+++ database/migrations/2024_06_04_142945_add_column_user.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at');
+            $table->timestamp('created_at');
         });
     }
 
