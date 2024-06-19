0cdb2f59973fffb8f8d65241c9532f3854097ecc
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-19 09:44:53 +0900

    usersテーブルにupdated_atとcreated_atにデフォルト値を追加

--- database/migrations/2024_06_04_142945_add_column_user.php
+++ database/migrations/2024_06_04_142945_add_column_user.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,8 +13,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('updated_at');
-            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
