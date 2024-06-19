adaf62e950aeee02b939dc21089255a5e4b175e0
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-19 16:04:58 +0900

    セッションテーブルの作成

--- database/migrations/2024_06_19_160429_create_sessions_table.php
+++ database/migrations/2024_06_19_160429_create_sessions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('sessions', function (Blueprint $table) {
+        Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
@@ -26,8 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('sessions', function (Blueprint $table) {
-            Schema::dropIfExists('sessions');
-        });
+        Schema::dropIfExists('sessions');
     }
 };
