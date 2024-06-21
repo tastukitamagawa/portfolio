59acb2f3634bb9556dcd09be223b0a79db7d1d98
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-06-21 16:15:57 +0900

    formのhttps対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(env('APP_ENV') === 'production') {
+        if(config('app.env') === 'production'){
             URL::forceScheme('https');
         }
     }
